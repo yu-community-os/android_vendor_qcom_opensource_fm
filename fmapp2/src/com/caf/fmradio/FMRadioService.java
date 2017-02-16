837be8685e3af1b37836f44b122c3374adcf30f8
Kamal Negi <kamaln@codeaurora.org>
2017-02-16 18:29:41 +0530

    FM Radio: Don't register AUDIO_BECOMING_NOISY intent
    FM registers two intent listener for headset removal. Because of the
    intents recieved from both listeners, race condition happens. Even antenna
    is connected to device, FM app shows antenna as disconnected.
    Hence register one listener for headset removal.
    
    Change-Id: Ic6811652a2356a80d3ba3517bf7ea11f73328ed3

--- fmapp2/src/com/caf/fmradio/FMRadioService.java
+++ fmapp2/src/com/caf/fmradio/FMRadioService.java
@@ -257,7 +257,6 @@ public class FMRadioService extends Service
       mSession.setFlags(MediaSession.FLAG_EXCLUSIVE_GLOBAL_PRIORITY |
                              MediaSession.FLAG_HANDLES_MEDIA_BUTTONS);
       mSession.setActive(true);
-      registerAudioBecomeNoisy();
       if ( false == SystemProperties.getBoolean("ro.fm.mulinst.recording.support",true)) {
            mSingleRecordingInstanceSupported = true;
       }
